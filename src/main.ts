c4ce97175bf4ae5a59aa50fb3f2a1e229710cd65
Elcho911 <boss.armsport@gmail.com>
2024-01-02 00:29:43 +0600

    fix bigbug host

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(5000);
+	const PORT: any = process.env.PORT || 3000;
+	try {
+		await app.listen(PORT, '0.0.0.0');
+
+		console.log(`${new Date()}`);
+		console.log('server running at: http://localhost:' + PORT);
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
 }
 bootstrap();

d696279951325e5eedecc8e490e421608d7b5bbb
Elcho911 <boss.armsport@gmail.com>
2024-01-03 22:49:12 +0600

    update prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CrudModule } from './crud/crud.module';
 import { TaskModule } from './task/task.module';
 
 @Module({
-	imports: [CrudModule, TaskModule],
+	imports: [DatabaseModule, CrudModule, TaskModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
